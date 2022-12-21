b565db5138bc78bb31eabf5eb29494d4c136b238
Everton <everton.aaraujo@outlook.com>
2022-12-21 17:56:58 -0300

    Style on registration page

--- app/src/Form/RegistrationFormType.php
+++ app/src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,19 +18,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Você deve aceitar os termos.',
-                    ]),
-                ],
+            ->add('name', TextType::class, [
+                'label' => 'Nome', 
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email', 
+                'attr' => ['class' => 'form-control mb-3']
             ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Senha', 
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control mb-3'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -38,6 +38,16 @@ class RegistrationFormType extends AbstractType
                         'min' => 6,
                         'minMessage' => 'Sua senha deve conter ao menos {{ limit }} characters',
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Termos de uso', 
+                'attr' => ['class' => 'mb-3 checkbox'],
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Você deve aceitar os termos.',
                     ]),
                 ],
             ])
