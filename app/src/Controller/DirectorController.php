0494f68fa62f90c6930423d6edf38601472d8912
Everton <everton.aaraujo@outlook.com>
2022-12-21 15:58:49 -0300

    Login created

--- app/src/Controller/DirectorController.php
+++ app/src/Controller/DirectorController.php
@@ -6,11 +6,13 @@ use App\Entity\Director;
 use App\Form\Type\DirectorType;
 use App\Repository\DirectorRepository;
 use App\Service\DirectorService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/director', name: 'director_')]
 class DirectorController extends AbstractController
 {
